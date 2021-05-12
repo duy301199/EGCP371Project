26491aa419781fba68dd32c737462c04f62e8d8d
Paul Nguyen <56055361+I-Eat-Dodos@users.noreply.github.com>
2021-05-11 19:50:36 -0700

    Update full_code.m

--- full_code.m
+++ full_code.m
@@ -9,39 +9,39 @@ b = 'trapPiano.wav';
 c = 'StarWars60.wav';
 d = 'Concerto.wav';
 
-%Display the menu of choices
+% Display the menu of choices
 choice = menu('Choose an option for type of song','a. Rock','b. Trap','c. StarWar','d. Concerto');
 
-%If the audience wants to choose Rock music
+% If the audience wants to choose Rock music
 if (choice == 1)
     [y,Fs] = audioread('Rockm.wav');
     info = audioinfo('Rockm.wav');
 a = 'Original Rock ';
 
-%If the audience wants to choose Trap music
+% If the audience wants to choose Trap music
 elseif (choice == 2)
     [y,Fs] = audioread('trapPiano.wav'); 
     info = audioinfo('trapPiano.wav');
  a = 'Original Trap Piano ';
 
-%If the audience wants to choose StarWar music
+% If the audience wants to choose StarWar music
 elseif (choice == 3)
     [y,Fs] = audioread('StarWars60.wav'); 
     info = audioinfo('StarWars60.wav');
     	 a = 'Original Star War ';
      
 
-%If the audience wants to choose Concerto music  
+% If the audience wants to choose Concerto music  
 elseif (choice == 4)
     [y,Fs] = audioread('Concerto.wav'); 
     info = audioinfo('Concerto.wav');
  	 a = 'Original Concerto ';
 end
 
-%Use this to play the audio signal
-%%sound(y,Fs); 
 
-%Program then send the chosen audio file to FFT to display signals.
+% Add white gaussian noise to a signal
+%awgn(original signal, SNR, signal Power)
+noisy_signal = awgn(y,5,'measured');
 
 % Create a t vector that is as long as the duration of the audio file
 t = 0:seconds(1/Fs):seconds(info.Duration);
@@ -52,7 +52,11 @@ t = t(1:end-1);
 size = numel(t);
 y = y(1:size);
 
-%FFT of the original signal
+
+% Program then send the chosen audio file to FFT to display signals.
+
+%% FFT
+% FFT of the original signal
 L = length(t);
 yFFT = fft(y);
 P2_orig = abs(yFFT/L);
@@ -60,45 +64,89 @@ P1_orig = P2_orig(1:L/2+1);
 P1_orig(2:end-1) = 2*P1_orig(2:end-1);
 f = Fs*(0:(L/2))/L;
 
-%Plots the wave of the audio file
-figure(1);
+
+% FFT of the noisy signal
+noisyFFT = fft(noisy_signal);
+P2_noisy = abs(noisyFFT/L);
+P1_noisy = P2_noisy(1:L/2+1);
+P1_noisy(2:end-1) = 2*P1_noisy(2:end-1);
+
+% This helps graph the cleaned signal
+P2_noisy2 = abs(noisyFFT);
+
+
+%% Filter out the noise
+threshold = 300; % Threshold choices which signals to remove
+selector = P2_noisy2 > threshold; % Anything below the threshold is removed
+P2_clean = P2_noisy2.*selector;
+Y_clean = selector.*noisyFFT;
+inv = ifft(Y_clean,L);
+
+P1_clean = P2_clean(1:L/2+1);
+P1_clean(2:end-1) = 2*P1_clean(2:end-1);
+
+%% Plots
+% Plots the wave of the audio file
+subplot(3,2,1);
 plot(t,y)
-title(['Signal of ',num2str(a),'Music'])
+title('Signal of Original Audio')
 xlabel('Time (t)')
 ylabel('Audio Signal')
 grid on
 
-%Plots the amplitude spectrum of the original signal
-figure(2);
-plot(f,P1_orig ) 
-title(['Amplitude Spectrum of ',num2str(a),'Music'])
+
+% Plots the noisy signal
+subplot(3,2,3);
+plot(t,noisy_signal)
+title('Signal of Noisy Audio')
+xlabel('Time (t)')
+ylabel('Audio Signal')
+grid on
+
+
+% Plots the wave of the clean audio
+subplot(3,2,5);
+plot(t,inv)
+title('Signal of Clean Audio')
+xlabel('Time (t)')
+ylabel('Audio Signal')
+grid on
+
+
+% Plots the amplitude spectrum of the original signal
+subplot(3,2,2);
+plot(f,P1_orig) 
+title('Amplitude Spectrum of Original Audio')
 xlabel('f (Hz)')
 ylabel('Amplitude')
 grid on
 
-%add white gaussian noise to a signal
-%awgn(original signal, SNR, signal Power)
-noisy_signal = awgn(y,5,'measured');
-%%Play the noise if you want to see how much was added 
-%%sound(noisy_signal,Fs)
 
-plot(t,noisy_signal);
-title(['Noisy Signal of  ',num2str(a),'Music'])
-xlabel('Time (t)')
-ylabel('Audio Signal')
+% Plots the amplitude spectrum of the noisy signal
+subplot(3,2,4);
+plot(f,P1_noisy) 
+title('Amplitude Spectrum of Noisy Audio')
+xlabel('f (Hz)')
+ylabel('Amplitude')
 grid on
 
 
-%% Create audio file of noisy signal
-filename = 'noisy_audio1.wav';
-audiowrite(filename,noisy_signal,Fs);
+% Plots the amplitude spectrum of the clean signal
+subplot(3,2,6);
+plot(f,P1_clean) 
+title('Amplitude Spectrum of Clean Audio')
+xlabel('f (Hz)')
+ylabel('Amplitude')
+grid on
 
 
-%% Parameters
+%% WAVELET TRANSFORM
+% Parameters
 method = 'Sure'; % Denoising Method: Bayes, BlockJS, FDR, Minimax, Sure, or UniversalThreshold
 wname = 'sym8'; % Name of Wavelet: haar, dbN, fkN, coifN, or symN where N is a positive integer
 level = 5; % Keep this at 5
 rule = 'Soft'; % Threshold Rule(depends on denoising method): BlockJS(James-Stein); Sure,Minimax,Universal Threshold(Soft or Hard); Bayes(Median, Mean, Soft, or Hard); FDR(Hard)
+
 
 %% Decompose noisy signal using Discrete Wavelet Transform
 dwtmode('per','nodisplay');
@@ -110,8 +158,9 @@ cd3 = wrcoef('d',C,L,wname,3);
 cd2 = wrcoef('d',C,L,wname,2);
 cd1 = wrcoef('d',C,L,wname,1);
 
+
 % Plot graphs of noisy signal
-figure(3);
+figure(2);
 subplot(6,3,1)
 plot(approx)
 title('Noisy Approximation Coefficients')
@@ -130,6 +179,7 @@ title('Level 2 Detail Coefficients')
 subplot(6,3,16)
 plot(cd1)
 title('Level 1 Detail Coefficients')
+
 
 %% Decompose original signal
 [C1, L1] = wavedec(y,level,wname);
@@ -171,11 +221,6 @@ cd14 = wrcoef('d',C2,L2,wname,4);
 cd13 = wrcoef('d',C2,L2,wname,3);
 cd12 = wrcoef('d',C2,L2,wname,2);
 cd11 = wrcoef('d',C2,L2,wname,1);
-
-
-%% Create audio file of clean signal
-filename = 'clean_audio1.wav';
-audiowrite(filename,fd,Fs);
 
 
 %% Plot graphs of clean signal
